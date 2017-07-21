8279d0f9110072fdc48c4537d9b453ac35198bf9
Matheus Filipe Teixeita Chaves <matheus.02468@gmail.com>
2017-07-21 17:43:44 -0300

    Adjusted the home controller.

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,11 @@
 
 var app = angular.module("lolipop");
 
-app.controller("homeController", function ($scope) {
+app.controller("homeController", function ($scope, Product) {
 
+    $scope.products;
+
+    Product.findAll().then(function (response) {
+        $scope.products = response.data;
+    });
 });
